603cc55ac5b79192f009af79ca9bc132efcfa1a4
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-05-23 14:19:03 +0100

    added JS to click and preform visible basic calculations

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 ((d) => {
 
-    
+    let equals = d.getElementById("equals");
+    let result = d.getElementById("result");
+    let input = d.getElementById("input");
+    let numbers = d.querySelectorAll(".printable");
+
+    currentResult = "";
+
+    equals.addEventListener("click", () => {
+        result.textContent = eval(+currentResult + input.value);
+        currentResult = result.textContent;
+        input.value = "";
+    });
+
+
+    numbers.forEach(number => {
+        number.addEventListener('click', () => {
+            input.value += number.textContent;
+        })
+    })
 
 })(document);
