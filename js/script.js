27a195aa8418eb51870771ee962d712ac8f38b22
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-05-23 14:58:53 +0100

    added a backspace option and changed the reset to give '0' as a starting point

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
 ((d) => {
 
-    let equals = d.getElementById("equals");
+
     let input = d.getElementById("input");
-    let numbers = d.querySelectorAll(".printable");
-    let reset = d.getElementById("reset");
-    let backspace = d.getElementById("backspace");
+
+
+    //Calculate the current expression
+    let equals = d.getElementById("equals");
 
     equals.addEventListener("click", () => {
         input.value = eval(input.value);
     });
 
+    //Delete last printed character
+    let backspace = d.getElementById("backspace");
+    backspace.addEventListener("click", () => {
+        input.value = input.value.slice(0, -1);
+    })
+
+    //Reset the calculator
+    let reset = d.getElementById("reset");
+
     reset.addEventListener("click", () => {
-        input.value = "";
+        input.value = "0";
     });
+
+
+    //Make the numbers and operators appear in the input
+    let numbers = d.querySelectorAll(".printable");
 
     numbers.forEach(number => {
         number.addEventListener('click', () => {
-            input.value += number.textContent;
+
+            if (input.value === "0") {
+                input.value = number.textContent;
+
+            } else {
+                input.value += number.textContent;
+            }
         })
     })
 
