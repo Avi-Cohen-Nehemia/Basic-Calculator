d111b52a958824627fd14c8f05e5fe5ac4fd99d0
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-05-23 14:40:25 +0100

    deleted the seperate 'result' and made it appear in the input instead

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
 ((d) => {
 
     let equals = d.getElementById("equals");
-    let result = d.getElementById("result");
     let input = d.getElementById("input");
     let numbers = d.querySelectorAll(".printable");
     let reset = d.getElementById("reset");
 
-    currentResult = "";
-
     equals.addEventListener("click", () => {
-        result.textContent = eval(+currentResult + input.value);
-        currentResult = result.textContent;
-        input.value = "";
+        input.value = eval(input.value);
     });
 
     reset.addEventListener("click", () => {
-        currentResult = "";
-        result.textContent = "0";
         input.value = "";
     });
 
